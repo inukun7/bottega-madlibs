29ef206fb35548d4133315b5f8f4c447c09ea52a
inukun7 <inukun716@gmail.com>
2018-07-10 11:47:40 -0600

    built our first Fuctional Component

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+    const Header = () => {
+    return (
+        <div className="header">
+            <h1>Bottega Madlibs</h1>
+            <p>
+                Fill out the fields below to click the generate button to see the Madlib story.
+            </p>
+        </div>
+    )
+}
+
+export default Header;
