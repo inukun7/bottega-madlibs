7ecfc62c0b9fd9f43ae94dff24653603b95da905
inukun7 <inukun716@gmail.com>
2018-07-11 12:26:29 -0600

    built our first class component for the inputs container, and built an input functional componet

---
+++ src/components/input.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+    const  Input = (name) => {
+    return (
+        <div className="input">
+            <input />
+            <label>{name}</label>
+        </div>
+    )
+}
+
+export default Input;
